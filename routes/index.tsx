ae0e755375b7b3d7594afab0f0cc6520062152e0
OleSkaar <olejohska@gmail.com>
2022-10-16 15:01:21 +0200

    Set up thanks page content, compress castle image

--- routes/index.tsx
+++ routes/index.tsx
@@ -9,48 +9,47 @@ export default function Home() {
       </Head>
       <Layout>
         <section class="card">
-          <section class="intro">
-            <h1>Ole & Nae</h1>
-            <p class="heading-sub">are getting married!</p>
-            <div class="info">
-              <time>
-                <strong>July 1st 2023</strong>
-              </time>
-              <p>Osaka Geihinkan, Japan</p>
-              <img class="image" src="/castle.png" />
-            </div>
-            <div class="info">
-              <p>Invitation to follow.</p>
-              <form action="/thanks" method="POST">
-                <input
-                  name="firstName"
-                  id="firstName"
-                  autoComplete="given-name"
-                  placeholder="First name"
-                  type="text"
-                  required
-                />
-                <input
-                  name="lastName"
-                  id="lastName"
-                  autoComplete="family-name"
-                  placeholder="Last name"
-                  type="text"
-                  required
-                />
-                <input
-                  name="email"
-                  type="email"
-                  id="email"
-                  autoComplete="email"
-                  placeholder="E-mail"
-                  required
-                />
-                <button>Submit</button>
-              </form>
-              <p>We hope you're able to attend!</p>
-            </div>
-          </section>
+          <h1>Ole & Nae</h1>
+          <p class="heading-sub">are getting married!</p>
+          <div class="info">
+            <time>
+              <strong>July 1st 2023</strong>
+            </time>
+            <p>Osaka Geihinkan, Japan</p>
+            <img class="image" src="/castle.png" />
+          </div>
+          <div class="info">
+            <p>Invitation to follow.</p>
+            <p>Send us your email so we know how to reach you.</p>
+            <form action="/thanks" method="POST">
+              <input
+                name="firstName"
+                id="firstName"
+                autoComplete="given-name"
+                placeholder="First name"
+                type="text"
+                required
+              />
+              <input
+                name="lastName"
+                id="lastName"
+                autoComplete="family-name"
+                placeholder="Last name"
+                type="text"
+                required
+              />
+              <input
+                name="email"
+                type="email"
+                id="email"
+                autoComplete="email"
+                placeholder="E-mail"
+                required
+              />
+              <button>Submit</button>
+            </form>
+            <p>We hope you're able to attend!</p>
+          </div>
         </section>
       </Layout>
     </>
--- routes/thanks.tsx
+++ routes/thanks.tsx
@@ -1,81 +1,88 @@
 import { Head } from "https://deno.land/x/fresh@1.1.1/runtime.ts";
-import { Handlers } from "https://deno.land/x/fresh@1.1.1/server.ts";
+import { Handlers, PageProps } from "https://deno.land/x/fresh@1.1.1/server.ts";
 import Layout from "../components/Layout.tsx";
 import { config } from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
 import { Client } from "https://deno.land/x/notion_sdk@v1.0.4/src/mod.ts";
 
+interface Data {
+    email: string;
+}
+
 export const handler: Handlers = {
   async POST(req, ctx) {
     const body = await req.formData();
-    
-    const email = body.get('email')?.toString();
-    const firstName = body.get('firstName')?.toString();
-    const lastName = body.get('lastName')?.toString();
-    
+
+    const email = body.get("email")?.toString();
+    const firstName = body.get("firstName")?.toString();
+    const lastName = body.get("lastName")?.toString();
+
     config({ export: true });
 
     const notion = new Client({
-        auth: Deno.env.get("NOTION_TOKEN"),
-    })
+      auth: Deno.env.get("NOTION_TOKEN"),
+    });
 
     async function addItem() {
-        try {
-          const response = await notion.pages.create({
-            parent: { database_id: Deno.env.get("NOTION_DATABASE_ID") as string },
-            properties: {
-              title: { 
-                title:[
-                  {
-                    "text": {
-                      "content": email ?? ''
-                    }
-                  }
-                ]
-              },
-              "First name": {
-                rich_text: [
-                    {
-                        "text": {
-                            "content": firstName ?? ''
-                        }
-                    }
-                ]
-              },
-              "Last name": {
-                rich_text: [
-                    {
-                        "text": {
-                            "content": lastName ?? ''
-                        }
-                    }
-                ]
-              }
+      try {
+        const response = await notion.pages.create({
+          parent: { database_id: Deno.env.get("NOTION_DATABASE_ID") as string },
+          properties: {
+            title: {
+              title: [
+                {
+                  "text": {
+                    "content": email ?? "",
+                  },
+                },
+              ],
             },
-          })
-          console.log(response)
-          console.log("Success! Entry added.")
-        } catch (error) {
-          console.error(error.body)
-        }
+            "First name": {
+              rich_text: [
+                {
+                  "text": {
+                    "content": firstName ?? "",
+                  },
+                },
+              ],
+            },
+            "Last name": {
+              rich_text: [
+                {
+                  "text": {
+                    "content": lastName ?? "",
+                  },
+                },
+              ],
+            },
+          },
+        });
+        console.log(response);
+        console.log("Success! Entry added.");
+      } catch (error) {
+        console.error(error.body);
       }
-      
-      addItem()
-    
-    const resp = await ctx.render();
-    
+    }
+
+    addItem();
+
+    const resp = await ctx.render({ email });
+
     return resp;
   },
 };
 
-export default function Thanks() {
+export default function Thanks({ data }: PageProps<Data>) {
   return (
     <>
       <Head>
       </Head>
       <Layout>
-        <h1>Thanks!</h1>
+        <section className="card">
+          <h1>Thank you!</h1>
+          <p>Your invitation will be sent to:</p>
+          <address>{data.email}</address>
+        </section>
       </Layout>
     </>
   );
 }
-
