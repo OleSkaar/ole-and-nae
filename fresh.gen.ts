6c31bac30de1a21a6b76d448c58cb7c3e4e163e2
OleSkaar <olejohska@gmail.com>
2023-06-05 23:23:43 +0200

    Update fresh.gen.ts

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -3,28 +3,32 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import config from "./deno.json" assert { type: "json" };
-import * as $0 from "./routes/city-hall-ceremony.tsx";
-import * as $1 from "./routes/index.tsx";
-import * as $2 from "./routes/info.tsx";
-import * as $3 from "./routes/invitation/[id].tsx";
-import * as $4 from "./routes/photos.tsx";
-import * as $5 from "./routes/response.tsx";
-import * as $6 from "./routes/save-the-date.tsx";
-import * as $7 from "./routes/thanks.tsx";
-import * as $8 from "./routes/upload.tsx";
+import * as $0 from "./routes/afterparty-jp-response.tsx";
+import * as $1 from "./routes/afterparty-jp.tsx";
+import * as $2 from "./routes/city-hall-ceremony.tsx";
+import * as $3 from "./routes/index.tsx";
+import * as $4 from "./routes/info.tsx";
+import * as $5 from "./routes/invitation/[id].tsx";
+import * as $6 from "./routes/photos.tsx";
+import * as $7 from "./routes/response.tsx";
+import * as $8 from "./routes/save-the-date.tsx";
+import * as $9 from "./routes/thanks.tsx";
+import * as $10 from "./routes/upload.tsx";
 import * as $$0 from "./islands/LanguageButton.tsx";
 
 const manifest = {
   routes: {
-    "./routes/city-hall-ceremony.tsx": $0,
-    "./routes/index.tsx": $1,
-    "./routes/info.tsx": $2,
-    "./routes/invitation/[id].tsx": $3,
-    "./routes/photos.tsx": $4,
-    "./routes/response.tsx": $5,
-    "./routes/save-the-date.tsx": $6,
-    "./routes/thanks.tsx": $7,
-    "./routes/upload.tsx": $8,
+    "./routes/afterparty-jp-response.tsx": $0,
+    "./routes/afterparty-jp.tsx": $1,
+    "./routes/city-hall-ceremony.tsx": $2,
+    "./routes/index.tsx": $3,
+    "./routes/info.tsx": $4,
+    "./routes/invitation/[id].tsx": $5,
+    "./routes/photos.tsx": $6,
+    "./routes/response.tsx": $7,
+    "./routes/save-the-date.tsx": $8,
+    "./routes/thanks.tsx": $9,
+    "./routes/upload.tsx": $10,
   },
   islands: {
     "./islands/LanguageButton.tsx": $$0,
